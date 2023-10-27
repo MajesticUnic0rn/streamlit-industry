fd7b5c419a4efaa9afaed1a18516c46e57bd5e30
Chris Dong <chrisdong@MacBook-Pro.local>
2023-10-26 22:27:30 -0400

    adding marketing sankey for key lead

--- pages/marketing.py
+++ pages/marketing.py
@@ -3,6 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 
 # Page Config
 st.set_page_config(page_title="Marketing Dashboard", page_icon="📉")
@@ -58,6 +59,40 @@ st.plotly_chart(fig1)
 fig2 = px.bar(audience_data, x='Platform', y='Audience', title="Social Media Target Audience", color='Platform')
 st.plotly_chart(fig2)
 
-st.write("Note: All data presented here is mock data and for demonstration purposes only.")
+# Mock Social Media Audience Data
+platforms = ["Facebook", "Instagram", "Twitter", "LinkedIn", "TikTok"]
+audiences = np.random.randint(5000, 50000, size=(len(platforms)))
 
-# Run this script to see the Streamlit dashboard for the marketing industry.
+# Conversion Rates (randomly generated for illustration)
+lead_conversion_rates = np.random.uniform(0.1, 0.4, len(platforms))
+loyal_conversion_rates = np.random.uniform(0.1, 0.3, len(platforms))
+
+# Calculating conversions
+leads_from_platforms = (audiences * lead_conversion_rates).astype(int)
+loyal_customers_from_leads = (leads_from_platforms * loyal_conversion_rates).astype(int)
+
+# Data for Sankey Diagram
+labels = platforms + ["Leads"] * len(platforms) + ["Loyal Customers"] * len(platforms)
+source = (list(range(len(platforms))) * 2)  # Each platform connects to its respective "Leads" and "Loyal Customers" nodes
+target = [len(platforms) + i for i in range(len(platforms))] + [len(platforms) * 2 + i for i in range(len(platforms))]
+value = list(leads_from_platforms) + list(loyal_customers_from_leads)
+
+# Create the Sankey diagram
+fig = go.Figure(data=[go.Sankey(
+    node=dict(
+        pad=15,
+        thickness=20,
+        line=dict(color="black", width=0.5),
+        label=labels
+    ),
+    link=dict(
+        source=source,
+        target=target,
+        value=value
+    )
+)])
+
+fig.update_layout(title_text="Social Media Audience to Loyal Customer Flow", font_size=15)
+
+# Display in Streamlit
+st.plotly_chart(fig)
